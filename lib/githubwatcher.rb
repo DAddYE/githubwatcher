4d6e8ee3e18d03c33e2b1dc06824df4e60e4800c
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2011-07-14 14:58:08 +0200

    Small code refactoring.

--- lib/githubwatcher.rb
+++ lib/githubwatcher.rb
@@ -12,9 +12,9 @@ module Githubwatcher
   base_uri 'https://api.github.com'
   format :json
 
-  def start!
+  def setup
     raise "You need to install growlnotify, use brew install growlnotify or install it from growl.info site" unless Growl.installed?
-    log "Starting GitHub Watcher..."
+    puts "Starting GitHub Watcher..."
 
     unless File.exist?(WATCH)
       warn "Add the repositories you're willing to monitor editing: ~/.githubwatcher/repos.yaml"
@@ -23,57 +23,56 @@ module Githubwatcher
     end
 
     @_watch = YAML.load_file(WATCH)
-
-    notify("GitHub Watcher", "is now listening...")
     @_repos = YAML.load_file(DB) if File.exist?(DB)
-    while !@_stop do
-      repos_was = repos.dup
-      watch.each do |value|
-        key, value = *value.split("/")
-        r = get "/users/%s/repos" % key
-        r.each do |repo|
-          next unless value.include?(repo["name"]) || value.include?("all")
-          log "Quering #{repo["git_url"]}..."
-
-          found = repos_was.find { |r| r["name"] == repo["name"] }
-
-          repo_fullname = [repo['owner']['login'],repo['name']].join('/')
-          if !found
-            notify(repo_fullname, "Was created")
-            repos_was << repo
-            repos << repo
-          end
-
-          repo_was = repos_was.find { |r| r["name"] == repo["name"] }
-
-          if repo_was["watchers"] != repo["watchers"]
-            notify(repo_fullname, "Has new #{repo["watchers"]-repo_was["watchers"]} watchers")
-            repo_was["watchers"] = repo["watchers"]
-          end
-
-          if repo_was["open_issues"] != repo["open_issues"]
-            notify(repo_fullname, "Has new #{repo["open_issues"]-repo_was["open_issues"]} open issues")
-            repo_was["open_issues"] = repo["open_issues"]
-          end
-
-          if repo_was["pushed_at"] != repo["pushed_at"]
-            notify(repo_fullname, "Was updated!")
-            repo_was["pushed_at"] = repo["pushed_at"]
-          end
-
-          if repo_was["forks"] != repo["forks"]
-            notify(repo_fullname, "Has new #{repo["forks"]-repo_was["forks"]} forks")
-            repo_was["forks"] = repo["forks"]
-          end
-
-          found = repo if found
-        end
-      end
-      Dir.mkdir(File.dirname(DB)) unless File.exist?(File.dirname(DB))
-      File.open(DB, "w"){ |f| f.write @_repos.to_yaml }
-      sleep 5
-    end
   end
+
+  def start!
+    repos_was = repos.dup
+    watch.each do |value|
+      key, value = *value.split("/")
+      r = get "/users/%s/repos" % key
+      r.each do |repo|
+        next unless value.include?(repo["name"]) || value.include?("all")
+        puts "Quering #{repo["git_url"]}..."
+
+        found = repos_was.find { |r| r["name"] == repo["name"] }
+
+        repo_fullname = [repo['owner']['login'],repo['name']].join('/')
+        if !found
+          notify(repo_fullname, "Was created")
+          repos_was << repo
+          repos << repo
+        end
+
+        repo_was = repos_was.find { |r| r["name"] == repo["name"] }
+
+        if repo_was["watchers"] != repo["watchers"]
+          notify(repo_fullname, "Has new #{repo["watchers"]-repo_was["watchers"]} watchers")
+          repo_was["watchers"] = repo["watchers"]
+        end
+
+        if repo_was["open_issues"] != repo["open_issues"]
+          notify(repo_fullname, "Has new #{repo["open_issues"]-repo_was["open_issues"]} open issues")
+          repo_was["open_issues"] = repo["open_issues"]
+        end
+
+        if repo_was["pushed_at"] != repo["pushed_at"]
+          notify(repo_fullname, "Was updated!")
+          repo_was["pushed_at"] = repo["pushed_at"]
+        end
+
+        if repo_was["forks"] != repo["forks"]
+          notify(repo_fullname, "Has new #{repo["forks"]-repo_was["forks"]} forks")
+          repo_was["forks"] = repo["forks"]
+        end
+
+        found = repo if found
+      end
+    end
+    Dir.mkdir(File.dirname(DB)) unless File.exist?(File.dirname(DB))
+    File.open(DB, "w"){ |f| f.write @_repos.to_yaml }
+  end
+  alias :run :start!
 
   def repos
     @_repos ||= []
@@ -83,19 +82,8 @@ module Githubwatcher
     @_watch ||= []
   end
 
-  def stop!
-    puts "\n<= Exiting ..."
-    @_stop = true
-  end
-
-  def log(text="")
-    text  = "[%s] %s" % [Time.now.strftime("%d/%m %H:%M"), text.to_s]
-    text += "\n" unless text[-1] == ?\n
-    print text; $stdout.flush
-  end
-
   def notify(title, text)
     Growl.notify text, :title => title, :icon => File.expand_path("../../images/icon.png", __FILE__); sleep 0.2
-    log "=> #{title}: #{text}"
+    puts "=> #{title}: #{text}"
   end
 end
